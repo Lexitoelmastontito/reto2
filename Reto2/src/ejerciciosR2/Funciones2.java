84baeea293fedc9ac0301b2ff13f39a1f3fa1e9e
danielchuperca <ciuperciucd51@gmail.com>
2024-10-31 09:26:47 +0100

    Funciones de temperatura y volumen y los subcampos

--- Reto2/src/ejerciciosR2/Funciones2.java
+++ Reto2/src/ejerciciosR2/Funciones2.java
@@ -1,5 +1,68 @@
 package ejerciciosR2;
 
-public class Funciones2 {
+import java.util.Scanner;
 
+
+
+public class Funciones2 {
+	
+	public static double celsiusaFahrenheit(double temperatura ) {
+		return (temperatura * 9/5) + 32;
+		
+	}
+    public static double Fahrenheitcelsius (double temperatura ) {
+		return (temperatura- 32) * 5 / 9;
+		
+
+		
+	}
+     public static double litrosagalones (double volumen ) {
+    	return volumen/3.785;
+    	
+    }
+    public static double galonesalitros (double volumen ) {
+    	return volumen*3.785;
+    	
+    }
+    public static void menutemperatura(String texto, Scanner sc ) {
+    	int n=0;
+    	do {
+        System.out.println("1. celsius a Fahrenheit");
+    	System.out.println("2. Fahrenheit a celsius");
+    	System.out.println("0. salir");
+        n=Reto2.dimeEntero("Que temperatura quieres", sc);
+    	}while(n!=0);
+    	switch(n) {
+    	case 1:
+    		System.out.println(celsiusaFahrenheit(n));
+    	case 2:
+    		System.out.println(Fahrenheitcelsius(n));
+    		
+    	}
+    
+    	
+		
+    	
+    }
+    public static void menuvolumen(String texto, Scanner sc ) {
+    	int n=0;
+    	do {
+        System.out.println("1. litros a galones");
+    	System.out.println("2. galones a litros");
+    	System.out.println("0. salir");
+        n=Reto2.dimeEntero("Que temperatura quieres", sc);
+    	}while(n!=0);
+    	switch(n) {
+    	case 1:
+    		System.out.println(litrosagalones(n));
+    	case 2:
+    		System.out.println(galonesalitros(n));
+    		
+    	}
+    
+    	
+		
+    	
+    }
+    
 }
