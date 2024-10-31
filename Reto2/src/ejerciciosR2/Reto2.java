711280516ed2f4bf3b338a8f9da1df69cbd3cdf3
danielchuperca <ciuperciucd51@gmail.com>
2024-10-31 09:01:20 +0100

    funcion dimedouble

--- Reto2/src/ejerciciosR2/Reto2.java
+++ Reto2/src/ejerciciosR2/Reto2.java
@@ -29,4 +29,17 @@ public class Reto2 {
 			}
 		} while (true);
 }
+	public static double dimeDouble(String texto, Scanner sc) {
+		do {
+		try {
+		System.out.println(texto);
+		String s1=sc.nextLine();
+		double num=Double.parseDouble(s1);
+		return num;
+		}catch (Exception ex) {
+			System.out.println("Formato incorrecto");
+		}
+	}while(true);
+		
+	}
 }
