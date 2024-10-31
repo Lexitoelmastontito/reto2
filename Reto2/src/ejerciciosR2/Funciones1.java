69952631e3d50d282f45c5f1c9f4560dd0905ee3
Lexitoelmastontito <alexfizquierdo10@gmail.com>
2024-10-31 08:49:55 +0100

    creadas las funciones de KgLibras y librasKg

--- Reto2/src/ejerciciosR2/Funciones1.java
+++ Reto2/src/ejerciciosR2/Funciones1.java
@@ -4,17 +4,28 @@ public class Funciones1 {
 	
 	
 
-	double kilometro=0;
-	double millas=0;
 	
-	public static double kmMillas(double km) {
+	
+	public static double kmMillas(double kilometro) {
 		double millas1=0.621371;
-		return km*millas1;
+		return kilometro*millas1;
 	}
 	public static double millasKm(double millas) {
 		double kilometro1=1.60934;
 		return millas*kilometro1;
 	}
+	
+	public static double kgLibras(double kilos) {
+		double libras1=2.20462;
+		return kilos*libras1;
+	}
+	
+	
+	public static double libraskg(double libras){
+		double kilogramos1= 0.453592;
+		return libras*kilogramos1;
+	}
+	
 	
 	
 	
