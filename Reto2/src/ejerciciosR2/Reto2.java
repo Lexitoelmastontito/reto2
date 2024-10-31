9eaf8faf8fce5212c5ef6e1043c3aabbd2a36707
Lexitoelmastontito <alexfizquierdo10@gmail.com>
2024-10-31 08:59:14 +0100

    dimeEntero

--- Reto2/src/ejerciciosR2/Reto2.java
+++ Reto2/src/ejerciciosR2/Reto2.java
@@ -1,5 +1,7 @@
 package ejerciciosR2;
 
+import java.util.Scanner;
+
 public class Reto2 {
 
 	public static void main(String[] args) {
@@ -9,8 +11,22 @@ public class Reto2 {
 		System.out.println("2. Peso");
 		System.out.println("3. Temperatura");
 		System.out.println("4. Volumen");
+		System.out.println("0. Salir");
 		
 
 	}
+	public static int dimeEntero(String texto, Scanner sc) {
+		do {
+			try {
+				System.out.println(texto);
+				String s1 = sc.nextLine();
+				int num = Integer.parseInt(s1);
+				return num;
+			} catch (Exception e) {
+				System.out.println("Formato incorrecto");
+				;
 
+			}
+		} while (true);
+}
 }
